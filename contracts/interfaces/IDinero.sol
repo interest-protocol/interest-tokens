13f191e3eb75c94303d2613ec252943e8f61d492
Jose Cerqueira <jose@interestprotocol.com>
2022-08-16 13:05:52 +0100

    feat: update sol version

--- contracts/interfaces/IDinero.sol
+++ contracts/interfaces/IDinero.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity >=0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-IERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import '@openzeppelin/hardhat-upgrades';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.15',
+    version: '0.8.16',
     settings: {
       optimizer: {
         enabled: true,
